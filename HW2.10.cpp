aeb95beb5b2f7d4a97cf2b9453239f9415092108
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-09-11 11:35:09 -0400

    HW2.10.cpp

--- HW2.10.cpp
+++ HW2.10.cpp
@@ -13,32 +13,23 @@ Write a program that asks the user to input
 
  */
 #include <iostream>
-#include <iomanip> // For setting decimal precision
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    double gallons, miles_per_gallon, price_per_gallon;
-    
-    cout << "The number of gallons of gas in the tank: ";
-    cin >> gallons;
-    
-    cout << "Fuel efficiency in miles per gallon: ";
-    cin >> miles_per_gallon;
-    
-    cout << "The price of gas per gallon: ";
-    cin >> price_per_gallon;
-    
-    // Calculate cost per 100 miles
-    double cost_per_100miles = (100.0 / miles_per_gallon) * price_per_gallon;
-    
-    // Calculate the distance that can be traveled with the current gallons of gas
-    double distance = gallons * miles_per_gallon;
-    
-    // Set decimal precision for output
-    cout << fixed << setprecision(2);
-    
-    cout << "The cost per 100 miles is $" << cost_per_100miles << " and the car can go " << distance << " miles with the gas in the tank" << endl;
-    
-    return 0;
+	double gallons, miles_per_gallon, price_per_gallon;
+	cout << "The number of gallons of gas in the tank: ";
+	cin >> gallons;
+	cout << "Fuel efficiency in miles per gallon: ";
+	cin >> miles_per_gallon;
+	cout << "The price of gas per gallon: ";
+	cin >> price_per_gallon;
+
+	double cost_per_100miles;
+	cost_per_100miles = (100.0 / miles_per_gallon) * price_per_gallon;
+
+	double distance = gallons * miles_per_gallon;
+	cout << "The cost per 100 miles is" << " " << "$" << cost_per_100miles << " " << "and the car can go" << " " << distance << " " << "miles with the gas in the tank";
+
+	return 0;
 }
